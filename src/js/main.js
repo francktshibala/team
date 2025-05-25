eac752d6c3629cfa58524c6c9fdca389381ab88b
AzureFirehawk <madison.thomas094@gmail.com>
2025-05-25 12:24:12 -0500

    updated main script.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,9 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+import CartCount from "./CartCount.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const productList = new ProductList("Tents", dataSource, element);
-
-productList.init();
+document.addEventListener("headerfooterloaded", () => {
+    const cartCount = new CartCount(document.querySelector(".cart"));
+    cartCount.render();
+});

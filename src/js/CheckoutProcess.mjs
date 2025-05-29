46790e84d7f723d26dcd3622876c31fb1177050d
AzureFirehawk <madison.thomas094@gmail.com>
2025-05-29 11:05:43 -0500

    Checkout process setup

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,4 +1,31 @@
 import { getLocalStorage } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
+
+const services = new ExternalServices();
+function packageItems(items) {
+    const simplifiedItems = items.map((item) => {
+        console.log(item);
+        return {
+            id: item.Id,
+            name: item.Name,
+            price: item.FinalPrice,
+            quantity: 1
+        }
+    });
+
+    return simplifiedItems;
+}
+
+function formDataToJSON(formElement) {
+    const formData = new FormData(formElement);
+    const data = {};
+
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+
+    return data;
+}
 
 export default class CheckoutProcess {
     constructor(key, outputSelector)
@@ -14,17 +41,22 @@ export default class CheckoutProcess {
 
     init() {
         this.list = getLocalStorage(this.key) || [];
-
+        this.calculateItemSubtotal();
     }
 
     calculateItemSubtotal() {
-        const summary = document.querySelector(this.outputSelector + "#cartTotal");
-        const itemNumber = document.querySelector(this.outputSelector + "#num-items");
+        const summary = document.querySelector(
+            this.outputSelector + "#cartTotal"
+        );
+        const itemNumber = document.querySelector(
+            this.outputSelector + "#num-items"
+        );
 
         itemNumber.innerText = this.list.length;
+
         const amounts = this.list.map(item => item.FinalPrice);
         this.itemTotal = amounts.reduce((sum, item) => sum + item);
-        summary.innerText = this.itemTotal.toFixed(2);
+        summary.innerText = `$${this.itemTotal}`;
     }
 
     calcuateOrderTotal() {
@@ -46,5 +78,24 @@ export default class CheckoutProcess {
         tax.innerText = this.tax.toFixed(2);
         shipping.innerText = this.shipping.toFixed(2);
         orderTotal.innerText = this.orderTotal.toFixed(2);
+    }
+
+    async checkout(form) {
+        const formElement = document.forms["checkout"];
+        const order = formDataToJSON(formElement);
+
+        order.orderDate = new Date().toISOString();
+        order.items = packageItems(this.list);
+        order.orderTotal = this.orderTotal;
+        order.tax = this.tax;
+        order.shipping = this.shipping;
+        console.log(order);
+
+        try {
+            const response = await services.checkout(order);
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,5 +1,6 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import CartCount from "./CartCount.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";
 
 // Load header and footer
 loadHeaderFooter();
@@ -9,3 +10,19 @@ document.addEventListener("headerfooterloaded", () => {
   cartCount.render();
   cartCount.listenForUpdates();
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const order = new CheckoutProcess("so-cart", ".checkout-summary ");
+  order.init();
+});
+
+// document
+//   .querySelector("#zip")
+//   .addEventListener("blur", order.calculateOrderTotal.bind(order));
+
+// // listening for click on the button
+// document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+//   e.preventDefault();
+
+//   order.checkout();
+// });
